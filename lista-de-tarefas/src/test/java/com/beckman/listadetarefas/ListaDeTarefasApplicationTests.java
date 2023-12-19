e4cc633cdee5e8a099e7abc45318ca06ab7f0645
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2023-12-18 22:05:51 -0400

    Inicio

---
+++ lista-de-tarefas/src/test/java/com/beckman/listadetarefas/ListaDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beckman.listadetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
